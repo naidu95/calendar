5069244fad491433bf28ab773ccce25ea7c6ea20
kvanadev1gmail <koteswarao.kvana@gmail.com>
2019-01-18 11:38:49 +0530

    Heroku app changes

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/physicianscheduling'));
+app.use(express.static(__dirname + '/dist/jquerycal'));
 
 app.get('/*', function(req, res) {    
-res.sendFile(path.join(__dirname+'/dist/physicianscheduling/index.html'));
+res.sendFile(path.join(__dirname+'/dist/jquerycal/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
